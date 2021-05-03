09bda4be10e33594b408964be5889bd450eb233a
madhank93 <maddymadhan946@gmail.com>
2021-05-03 18:50:58 +0530

    feat: initial mongoose commit

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import express from 'express';
+import mongoose from 'mongoose';
 
 const app = express();
 
+mongoose
+    .connect('mongodb://admin:admin@172.25.0.2:27017/?authSource=admin', () => console.log('success now'))
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
