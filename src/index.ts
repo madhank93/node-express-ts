7cad6cdd6a60fcffe7ea236ecfa51a114c285ab9
madhank93 <maddymadhan946@gmail.com>
2021-05-04 10:00:35 +0530

    ref: added config and reffering properties from it

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,19 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { Config } from './config/config';
 
 const app = express();
 
+const mongoURL = `mongodb://${Config.MONGO_USER}:${Config.MONGO_PASSWORD}@${Config.MONGO_IP}:${Config.MONGO_PORT}/?authSource=admin`;
+
 mongoose
-    .connect('mongodb://admin:admin@172.25.0.2:27017/?authSource=admin', () => console.log('success now'))
+    .connect(mongoURL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+        useFindAndModify: false,
+    })
+    .then(() => console.log('Ran successfully'))
+    .catch((err) => console.log(err));
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
