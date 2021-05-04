41f9c6ee3135bdef6ca6faf1365059436f33c05e
madhank93 <maddymadhan946@gmail.com>
2021-05-04 21:56:25 +0530

    fix: added mongodb retry logic

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,37 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import { Config } from './config/config';
+import postRouter from './routes/postRoutes';
 
 const app = express();
 
 const mongoURL = `mongodb://${Config.MONGO_USER}:${Config.MONGO_PASSWORD}@${Config.MONGO_IP}:${Config.MONGO_PORT}/?authSource=admin`;
 
-mongoose
-    .connect(mongoURL, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        useFindAndModify: false,
-    })
-    .then(() => console.log('Ran successfully'))
-    .catch((err) => console.log(err));
+const connectWithRetry = () => {
+    mongoose
+        .connect(mongoURL, {
+            useNewUrlParser: true,
+            useUnifiedTopology: true,
+            useFindAndModify: false,
+        })
+        .then(() => console.log('Ran successfully'))
+        .catch((err) => {
+            console.log(err);
+            setTimeout(connectWithRetry, 5000);
+        });
+};
+
+connectWithRetry();
+
+app.use(express.json());
+
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
     res.send('<h2>Hi, there. This is Madhan K</h2>');
 });
+
+app.use('/api/v1/posts', postRouter);
 
 app.listen(port, () => {
     return console.log(`server is listening on ${port}`);
