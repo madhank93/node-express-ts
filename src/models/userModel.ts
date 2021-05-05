b61b79b8e329019c7e34ca02bf70c27ea7bfc569
madhank93 <maddymadhan946@gmail.com>
2021-05-05 23:25:38 +0530

    feat: added user model

---
+++ src/models/userModel.ts
@@ -0,0 +1,20 @@
+import { Model, Schema, model, Document } from 'mongoose';
+
+export interface UserDocument extends Document {
+    username: string;
+    password: string;
+}
+
+const userSchema: Schema = new Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: [true, 'Username is required'],
+    },
+    password: {
+        type: String,
+        required: [true, 'Password is required'],
+    },
+});
+
+export const User: Model<UserDocument> = model('User', userSchema);
